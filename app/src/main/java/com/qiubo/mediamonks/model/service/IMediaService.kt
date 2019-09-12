51ded62981f12dbc6caa1dfd92af1feacc059e42
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-12 16:04:54 -0300

    changed Call class for Observable

--- app/src/main/java/com/qiubo/mediamonks/model/service/IMediaService.kt
+++ app/src/main/java/com/qiubo/mediamonks/model/service/IMediaService.kt
@@ -1,13 +1,13 @@
 package com.qiubo.mediamonks.model.service
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import rx.Observable
 
 interface IMediaService {
     @GET("albums/")
-    fun getAlbums(): Call<List<Any>>
+    fun getAlbums(): Observable<List<Any>>
 
     @GET("albums/{albumId}/photos/{photoId}")
-    fun getPhotos(@Path("albumId") albumId: String, @Path("photoId") photoId: String = ""): Call<List<Any>>
+    fun getPhotos(@Path("albumId") albumId: String, @Path("photoId") photoId: String = ""): Observable<List<Any>>
 }
