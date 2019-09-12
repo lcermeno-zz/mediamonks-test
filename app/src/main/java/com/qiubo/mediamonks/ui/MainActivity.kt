5bc320caa7ec9bd7eb74937563bfdae8a799cb5a
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-12 14:51:14 -0300

    moved MainActivity to ui package

--- app/src/main/java/com/qiubo/mediamonks/ui/MainActivity.kt
+++ app/src/main/java/com/qiubo/mediamonks/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.qiubo.mediamonks
+package com.qiubo.mediamonks.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.qiubo.mediamonks.R
 
 class MainActivity : AppCompatActivity() {
 
