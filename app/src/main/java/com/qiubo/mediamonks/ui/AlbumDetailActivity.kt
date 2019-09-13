2abc4d64a17b4f1cfadd6a88aabbe70e53dcc792
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-13 16:26:55 -0300

    added AlbumDetailActivity

---
+++ app/src/main/java/com/qiubo/mediamonks/ui/AlbumDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.qiubo.mediamonks.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qiubo.mediamonks.R
+
+class AlbumDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_detail)
+    }
+}
