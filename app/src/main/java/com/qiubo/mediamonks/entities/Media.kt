74cdc8728f983ce6c1e3e0538501fd395c06e88e
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-13 20:07:01 -0300

    added Parcelize annotation to Photo data class

--- app/src/main/java/com/qiubo/mediamonks/entities/Media.kt
+++ app/src/main/java/com/qiubo/mediamonks/entities/Media.kt
@@ -6,10 +6,11 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Album(val id: Int, val userId: Int, val title: String) : Parcelable
 
+@Parcelize
 data class Photo(
     val id: Int,
     val title: String,
     val url: String,
     val thumbnailUrl: String,
     var size: Float = 0f
-)
+) : Parcelable
