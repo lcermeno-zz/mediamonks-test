34b85167ac9f616f48035847b38b9059fbcad8bf
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-13 11:41:12 -0300

    updated IMediaService signature

--- app/src/main/java/com/qiubo/mediamonks/api/service/IMediaService.kt
+++ app/src/main/java/com/qiubo/mediamonks/api/service/IMediaService.kt
@@ -1,13 +1,15 @@
 package com.qiubo.mediamonks.api.service
 
+import com.qiubo.mediamonks.entities.Album
+import com.qiubo.mediamonks.entities.Photo
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
-import rx.Observable
 
 interface IMediaService {
     @GET("albums/")
-    fun getAlbums(): Observable<List<Any>>
+    fun getAlbums(): Observable<List<Album>>
 
     @GET("albums/{albumId}/photos/{photoId}")
-    fun getPhotos(@Path("albumId") albumId: String, @Path("photoId") photoId: String = ""): Observable<List<Any>>
+    fun getPhotos(@Path("albumId") albumId: String, @Path("photoId") photoId: String = ""): Observable<List<Photo>>
 }
