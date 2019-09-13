a69c0587ed101095dde6f8c511e5f40a8b5b79ac
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-13 20:17:06 -0300

    updated AlbumDetailActivity integrated with its presenter

--- app/src/main/java/com/qiubo/mediamonks/ui/AlbumDetailActivity.kt
+++ app/src/main/java/com/qiubo/mediamonks/ui/AlbumDetailActivity.kt
@@ -1,13 +1,71 @@
 package com.qiubo.mediamonks.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.qiubo.mediamonks.R
+import com.qiubo.mediamonks.entities.Album
+import com.qiubo.mediamonks.entities.Photo
+import com.qiubo.mediamonks.misc.Constants
+import com.qiubo.mediamonks.misc.ScreenHelper
+import com.qiubo.mediamonks.model.domain.GetMediaUseCase
+import com.qiubo.mediamonks.presenter.AlbumDetailPresenter
+import com.qiubo.mediamonks.presenter.IAlbumDetailPresenter
+import com.qiubo.mediamonks.ui.adapters.PhotosAdapter
+import com.qiubo.mediamonks.view.IAlbumDetailView
+import kotlinx.android.synthetic.main.activity_album_detail.*
 
-class AlbumDetailActivity : AppCompatActivity() {
+class AlbumDetailActivity : AppCompatActivity(), IAlbumDetailView, PhotosAdapter.IOnClickListener {
+
+    private val mAdapter by lazy { PhotosAdapter(mutableListOf(), this) }
+    private lateinit var mPresenter: IAlbumDetailPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_album_detail)
+
+        albumDetailRecycler.layoutManager =
+            StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        albumDetailRecycler.adapter = mAdapter
+
+        albumDetailRefresh.setOnRefreshListener { mPresenter.getPhotos() }
+
+        val album = intent.getParcelableExtra<Album>(Constants.ITEM_KEY)
+
+        val width = ScreenHelper.getScreenRealWidth(this)
+
+        mPresenter = AlbumDetailPresenter(this, GetMediaUseCase(), album, width)
+        mPresenter.getPhotos()
+    }
+
+    override fun onError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onClickItem(
+        item: Photo,
+        pair: Pair<View, String>
+    ) {
+        val intent = Intent(this, FullscreenActivity::class.java)
+        intent.putExtra(Constants.ITEM_KEY, item)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+
+        val optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this, pair)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            startActivity(intent, optionsCompat.toBundle())
+        else
+            startActivity(intent)
+    }
+
+    override fun onGetItems(items: List<Photo>) {
+        mAdapter.setItems(items)
+        albumDetailRefresh.isRefreshing = false
     }
 }
