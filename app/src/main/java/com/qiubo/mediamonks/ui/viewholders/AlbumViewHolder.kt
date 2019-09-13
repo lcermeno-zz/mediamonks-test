d0d03e4c7db07c5f87e991a7bf531a212874d08d
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-13 15:45:07 -0300

    updated AlbumVieHolder to assign the album title

--- app/src/main/java/com/qiubo/mediamonks/ui/viewholders/AlbumViewHolder.kt
+++ app/src/main/java/com/qiubo/mediamonks/ui/viewholders/AlbumViewHolder.kt
@@ -4,11 +4,12 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.qiubo.mediamonks.entities.Album
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.recycler_album_item.*
 
 class AlbumViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
     LayoutContainer {
 
-    fun setValues(items: Album) {
-
+    fun setValues(item: Album) {
+        album_title.text = item.title
     }
 }
