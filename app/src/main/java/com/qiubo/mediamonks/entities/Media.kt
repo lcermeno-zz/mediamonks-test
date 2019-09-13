c67a40dcb01a1c04d1821e1554ae6ba0b9551a69
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-13 16:26:30 -0300

    made parcelable the Album class

--- app/src/main/java/com/qiubo/mediamonks/entities/Media.kt
+++ app/src/main/java/com/qiubo/mediamonks/entities/Media.kt
@@ -1,4 +1,8 @@
 package com.qiubo.mediamonks.entities
 
-data class Album(val id: Int, val userId: Int, val title: String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Album(val id: Int, val userId: Int, val title: String) : Parcelable
 data class Photo(val id: Int, val title: String, val url: String, val thumbnailUrl: String)
