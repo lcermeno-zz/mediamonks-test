2068a36d87e13494a1a28d01c0ca1e2318aac546
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-13 22:26:03 -0300

    added pagination keys

--- app/src/main/java/com/qiubo/mediamonks/misc/Constants.kt
+++ app/src/main/java/com/qiubo/mediamonks/misc/Constants.kt
@@ -2,4 +2,8 @@ package com.qiubo.mediamonks.misc
 
 object Constants {
     const val ITEM_KEY = "item"
+    const val LIMIT_KEY = "_limit"
+    const val PAGE_KEY = "_page"
+    const val ALBUM_ID_KEY = "albumId"
+    const val PAGE_LIMIT = 20
 }
